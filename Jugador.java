1155421a6637d8e453e9dd31da7977e6649d6d5e
Julian Pasos <jpasos@uabc.edu.mx>
2025-03-27 17:12:09 -0700

    se agregan metodos nuevos

--- Jugador.java
+++ Jugador.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * La clase jugador
@@ -7,33 +8,36 @@ import java.util.ArrayList;
  * @version (a version number or a date)
  */
 public class Jugador {
-    private ArrayList<Ficha> Mano;
+    private String nombre;
+    private ArrayList<Ficha> mano;
     private int puntuacion;
 
-    public Jugador() {
-        Mano = new ArrayList<>();
+    public Jugador(String nombre) {
+        mano = new ArrayList<>();
         puntuacion = 0;
+        this.nombre = nombre;
     }
 
     public int getPuntuacion() {
         return puntuacion;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
     public int getPuntosEnMano() {
         int puntos = 0;
-        for (int i = 0; i < Mano.size(); i++) {
-            puntos = puntos + Mano.get(i).getPuntosTotales();
+        for (int i = 0; i < mano.size(); i++) {
+            puntos = puntos + mano.get(i).getPuntosTotales();
         }
         return puntos;
     }
 
-    public int getNumDeFichas() {
-        return Mano.size();
+    public boolean tieneFichas() {
+        return !mano.isEmpty();
     }
 
-    public void reiniciarPuntuacion() {
-        puntuacion = 0;
-    }
 
     public void sumarPuntosEnMano() {
         puntuacion = puntuacion + getPuntosEnMano();
@@ -41,18 +45,104 @@ public class Jugador {
 
     public void recibirFichas(ArrayList<Ficha> fichasRecibidas) {
         for (int i = 0; i < fichasRecibidas.size(); i++) {
-            Mano.add(fichasRecibidas.get(i));
+            mano.add(fichasRecibidas.get(i));
         }
 
     }
 
-
     public void borrarMano() {
-        Mano.clear();
+        mano.clear();
     }
 
     public Ficha colocarFicha(int posicion) {
-        return Mano.remove(posicion);
+        return mano.remove(posicion);
     }
 
+    public boolean tieneDobleSeis() {
+        for (int i = 0; i < mano.size(); i++) {
+            if (mano.get(i).getCara1() == 6 && mano.get(i).getCara2() == 6) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getDobleMasAlto() {
+        int dobleMasAlto = 0;
+        for (int i = 0; i < mano.size(); i++) {
+            if (mano.get(i).esDoble()) {
+                if (mano.get(i).getCara1() > dobleMasAlto) {
+                    dobleMasAlto = mano.get(i).getCara1();
+                }
+            }
+        }
+        return dobleMasAlto;
+    }
+
+    public boolean tieneDoble(){
+        for (int i = 0; i < mano.size(); i++) {
+            if (mano.get(i).esDoble()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getValorDeFichaMasAlta() {
+        int valorMasAlto = 0;
+        for (int i = 0; i < mano.size(); i++) {
+            if (mano.get(i).getPuntosTotales() > valorMasAlto) {
+                valorMasAlto = mano.get(i).getPuntosTotales();
+            }
+
+        }
+        return valorMasAlto;
+    }
+
+    public void imprimirMano() {
+        for (int i = 0; i < mano.size(); i++) {
+            System.out.println(mano.get(i).toString());
+        }
+    }
+
+    public Ficha colocarFichaInicial(){
+        Ficha ficha = null;
+        boolean encontrado = false;
+        int dobleMasAlta = 0;
+        if(tieneDobleSeis()){
+            for (int i = 0; i < mano.size(); i++) {
+                if (mano.get(i).getCara1() == 6 && mano.get(i).getCara2() == 6) {
+                    ficha = mano.remove(i);
+                    encontrado = true;
+                }
+            }
+        }
+        else if(tieneDoble() && !encontrado){
+            for (int i = 0; i < mano.size(); i++) {
+                if (mano.get(i).esDoble() && mano.get(i).getCara1() > dobleMasAlta) {
+                    dobleMasAlta = mano.get(i).getCara1();
+                    ficha = mano.remove(i);
+                    encontrado = true;
+                }
+            }
+        }
+        else if (!encontrado){
+            for (int i = 0; i < mano.size(); i++) {
+                if (dobleMasAlta == mano.get(i).getPuntosTotales()) {
+                    ficha = mano.remove(i);
+                    encontrado = true;
+                }
+            }
+        }
+    return ficha;
+    }
+
+
+    public Ficha getFicha(int ficha) {
+        return mano.get(ficha);
+    }
+
+    public int getManoSize() {
+        return mano.size();
+    }
 }
