eaeca7536613c697dfa43cbf635b86cc0e101771
Julian Pasos <jpasos@uabc.edu.mx>
2025-03-27 17:11:18 -0700

    Se agrega metodo de que inicializa la ronda y otro que maneje los turnos

--- Matador.java
+++ Matador.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Write a description of class Matador here.
@@ -10,15 +11,15 @@ public class Matador {
     private BarajaDeFichas boneyard;
     private ArrayList<Jugador> jugadores;
     private Table table;
+    private int jugadorEnTurno;
+    private int puntuacionMaxima;
 
-    public Matador(int numJugadores) {
+    public Matador(int numJugadores, int puntuacionMaxima) {
         jugadores = new ArrayList<>(numJugadores);
         for (int i = 0; i < numJugadores; i++) {
-            jugadores.add(new Jugador());
+            jugadores.add(new Jugador("Jugador " + (i + 1)));
         }
-        table = new Table();
-
-
+        this.puntuacionMaxima = puntuacionMaxima;
     }
 
     //si numero de jugadores = 2 o 3, repartir 7 fichas a cada jugador
@@ -36,49 +37,124 @@ public class Matador {
         }
     }
 
-    public int getJugadorConMulaMayor() {
-        int mulaMayor = 0;
-        int jugador = 0;
+    public int getJugadorInicial() {
+        int jugadorInicial = 0;
+        int mulaAlta = 0;
+        int valorDeFichaMasAlta = 0;
+        boolean hayMulas = false;
+        boolean hayDobleSeis = false;
         for (int i = 0; i < jugadores.size(); i++) {
-            if (jugadores.get(i).getPuntosEnMano() > mulaMayor) {
-                mulaMayor = jugadores.get(i).getPuntosEnMano();
-                jugador = i;
+            if (jugadores.get(i).tieneDobleSeis()) {
+                hayDobleSeis = true;
+                jugadorInicial = i;
             }
+            else if (jugadores.get(i).tieneDoble() && !hayDobleSeis) {
+                hayMulas = true;
+                if (jugadores.get(i).getDobleMasAlto() > mulaAlta){
+                    mulaAlta = jugadores.get(i).getDobleMasAlto();
+                    jugadorInicial = i;
+                }
+            }
+            else if (!hayMulas && !hayDobleSeis) {
+                if (jugadores.get(i).getValorDeFichaMasAlta() > valorDeFichaMasAlta) {
+                    valorDeFichaMasAlta = jugadores.get(i).getValorDeFichaMasAlta();
+                    jugadorInicial = i;
+                }
+            }
+
         }
-        return jugador;
+        return jugadorInicial;
     }
 
-    public boolean unJugadorTiene0Fichas() {
-        for (Jugador jugadores : jugadores) {
-            if (jugadores.getNumDeFichas() == 0) {
+    public boolean jugadorLlegoAPuntuacionMaxima(){
+        for (Jugador jugador : jugadores) {
+            if (jugador.getPuntuacion() >= puntuacionMaxima) {
                 return true;
             }
         }
         return false;
     }
 
-    public void ronda() {
+    public void iniciarRonda(){
         boneyard = new BarajaDeFichas(6);
+        boneyard.barajar();
+        table = new Table();
+        for (Jugador jugador : jugadores) {
+            jugador.borrarMano();
+        }
         repartirFichas();
+        jugadorEnTurno = getJugadorInicial();
+        System.out.println("Jugador inicial: " + jugadores.get(jugadorEnTurno).getNombre() + " Con la mano:");
+        jugadores.get(jugadorEnTurno).imprimirMano();
+        table.recibirFicha(jugadores.get(jugadorEnTurno).colocarFichaInicial());
+        table.mostrarFichasJugadas();
+        acabarTurno();
 
-        int turnoDeJugador = getJugadorConMulaMayor();
-        do {
+    }
 
 
-        if (turnoDeJugador < jugadores.size()){
-            turnoDeJugador++;
-        } else {
-            turnoDeJugador = 0;
+    public void jugarTurno(){
+        int ficha = -1;
+        Scanner sc = new Scanner(System.in);
+        int manoSize = jugadores.get(jugadorEnTurno).getManoSize();
+        boolean fichaValida = false;
+        boolean manoValida = false;
+
+        System.out.println("Jugador " + jugadores.get(jugadorEnTurno).getNombre() + " con la mano:");
+        jugadores.get(jugadorEnTurno).imprimirMano();
+        while (!manoValida) {
+            for (int i = 0; i < jugadores.get(jugadorEnTurno).getManoSize(); i++) {
+                if (table.puedeColocarse(jugadores.get(jugadorEnTurno).getFicha(i))) {
+                    manoValida = true;
+                    break;
+                }
+            }
+            if (!manoValida) {
+                System.out.println("No puede colocar ninguna ficha, robando ficha");
+                jugadores.get(jugadorEnTurno).recibirFichas(boneyard.repartirFichas(1));
+            }
         }
-        } while (unJugadorTiene0Fichas());
+            while (!fichaValida) {
+                System.out.println("Que ficha desea colocar?");
+                ficha = sc.nextInt() - 1;
+                if (ficha < manoSize && ficha >= 0) {
+                    if (table.puedeColocarse(jugadores.get(jugadorEnTurno).getFicha(ficha))) {
+                        fichaValida = true;
+                    } else {
+                        System.out.println("La ficha: " + jugadores.get(jugadorEnTurno).getFicha(ficha).toString() + " no puede colocarse");
+                    }
+                } else {
+                    System.out.println("Ficha no válida, intente de nuevo");
+                }
+            }
+            table.recibirFicha(jugadores.get(jugadorEnTurno).colocarFicha(ficha));
+            table.mostrarFichasJugadas();
 
-        for (Jugador jugadores : jugadores) {
-            jugadores.sumarPuntosEnMano();
-        }
+        acabarTurno();
+    }
 
-        for (Jugador jugadores : jugadores) {
-            jugadores.borrarMano();
+    public void acabarTurno() {
+        jugadorEnTurno++;
+        if (jugadorEnTurno == jugadores.size()) {
+            jugadorEnTurno = 0;
         }
+    }
+
+    public boolean jugadorTieneFichas(){
+        return jugadores.get(jugadorEnTurno).tieneFichas();
+    }
+
+    // main
+    public static void main(String[] args){
+            Matador matador = new Matador(4, 100);
+            while (!matador.jugadorLlegoAPuntuacionMaxima()) {
+                matador.iniciarRonda();
+                while (matador.jugadorTieneFichas()) {
+                    matador.jugarTurno();
+                }
+                break;
+            }
+
     }
 }
 
